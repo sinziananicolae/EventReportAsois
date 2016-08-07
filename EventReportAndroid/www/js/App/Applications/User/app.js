ca50ed47bbaaba2424772782c355358ed3d27cf6
sinziananicolae <sinzianna.nicolae@gmail.com>
2016-08-07 14:54:28 +0300

    Setup of android project

---
+++ EventReportAndroid/www/js/App/Applications/User/app.js
@@ -0,0 +1,53 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("app", [
+            "ngRoute",
+            "ngSanitize",
+            "ngResource",
+            "ngAnimate",
+            //"services.notifications",
+            "services.httpRequestTracker",
+            "ui.bootstrap",
+            "app.admin",
+            "directivesTheme",
+            "directives",
+            "uiGmapgoogle-maps"
+        ])
+        .config([
+            "$routeProvider", "$locationProvider", "$httpProvider", function($routeProvider, $locationProvider, $httpProvider) {
+                $httpProvider.interceptors.push("myHttpInterceptor");
+            }
+        ]).config(function (uiGmapGoogleMapApiProvider) {
+            uiGmapGoogleMapApiProvider.configure({
+                key: "AIzaSyAMQFsBM2fWL0QHhDvkU9oIk1GC2LfWcww",
+                v: "3.20", //defaults to latest 3.X anyhow
+                libraries: "places"
+            });
+        }).factory("myHttpInterceptor", [
+            "$q", "$rootScope", "$location", function($q, $rootScope, $location) {
+                $rootScope.pendingRequests = 0;
+                return {
+                    'request': function(config) {
+                        $rootScope.pendingRequests++;
+                        return config || $q.when(config);
+                    },
+
+                    'requestError': function(rejection) {
+                        $rootScope.pendingRequests--;
+                        return $q.reject(rejection);
+                    },
+
+                    'response': function(response) {
+                        $rootScope.pendingRequests--;
+                        return response || $q.when(response);
+                    },
+
+                    'responseError': function(rejection) {
+                        $rootScope.pendingRequests--;
+                        return $q.reject(rejection);
+                    }
+                };
+            }
+        ]);
+})();
---
+++ EventReportAndroid/www/js/App/Applications/User/config.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    // Static
+    angular.module('config', [])
+        // API Urls
+        .constant("API", {
+            url: "http://localhost:39313/api",
+            root: "",
+            modules: "/js/App/Applications/User/Modules"
+        })
+        // Logger modules
+        .constant("LoggerModules", {
+            HomeController: "HomeController"
+        });
+}());
